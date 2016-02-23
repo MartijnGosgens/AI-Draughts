ab44a9b353fd7da1e7db323e5be88f6b799c88a7
MartijnGosgens <martijn.gosgens@hotmail.com>
2016-02-23 14:55:55 +0100

    Test gemaakt
    Hij bruteforcet en kijkt of het antwoord hetzelfde is.

--- 2ID90-Group-28/src/nl/tue/s2id90/group28/AlphaBetaPlayer.java
+++ 2ID90-Group-28/src/nl/tue/s2id90/group28/AlphaBetaPlayer.java
@@ -6,6 +6,7 @@
 package nl.tue.s2id90.group28;
 
 import java.util.List;
+import java.util.Objects;
 import nl.tue.s2id90.draughts.DraughtsState;
 import nl.tue.s2id90.draughts.player.DraughtsPlayer;
 import org10x10.dam.game.Move;
@@ -17,12 +18,18 @@ import org10x10.dam.game.Move;
 public class AlphaBetaPlayer extends DraughtsPlayer {
 
     private final int DEPTH_LIMIT = 4;
+    private boolean isBlack;
     
     @Override
     public Move getMove(DraughtsState s) {
         List<Move> moves = s.getMoves();
         Integer bestValue = Integer.MIN_VALUE;
         Move bestMove = moves.get(0);
+        
+        Integer bruteForceValue = bruteForceMax(s, 0);
+        
+        // See whether the player plays black or white
+        isBlack = moves.get(0).isBlackMove();
         for (Move m : moves) {
             s.doMove(m);
             Integer value = alphaBetaMax(s, Integer.MIN_VALUE, Integer.MAX_VALUE, 1);
@@ -31,6 +38,12 @@ public class AlphaBetaPlayer extends DraughtsPlayer {
                 bestMove = m;
             }
             s.undoMove(m);
+        }
+        
+        // Test whether alpha-beta gets the same value as brute force.
+        if (!Objects.equals(bruteForceValue, bestValue)) {
+            System.out.println("AlphaBeta does not work, brute force: "
+                    + bruteForceValue + ", alpha-beta: " + bestValue);
         }
         return bestMove;
     }
@@ -51,13 +64,13 @@ public class AlphaBetaPlayer extends DraughtsPlayer {
         } else {
             List<Move> moves = s.getMoves();
             for (Move m : moves) {
-               s.doMove(m);
-               Integer min = alphaBetaMin(s, alpha, beta, depth + 1);
-               alpha = (alpha > min ? alpha : min);
-               s.undoMove(m);
-               if (alpha >= beta) {
-                   return beta;
-               }
+                s.doMove(m);
+                Integer min = alphaBetaMin(s, alpha, beta, depth + 1);
+                alpha = (alpha > min ? alpha : min);
+                s.undoMove(m);
+                if (alpha >= beta) {
+                    return beta;
+                }
             }
             return alpha;
         }
@@ -70,16 +83,47 @@ public class AlphaBetaPlayer extends DraughtsPlayer {
         } else {
             List<Move> moves = s.getMoves();
             for (Move m : moves) {
-               s.doMove(m);
-               Integer max = alphaBetaMax(s, alpha, beta, depth + 1);
-               beta = (beta > max ? beta : max);
-               s.undoMove(m);
-               if (alpha >= beta) {
-                   return alpha;
-               }
-            }
+                s.doMove(m);
+                Integer max = alphaBetaMax(s, alpha, beta, depth + 1);
+                beta = (beta > max ? beta : max);
+                s.undoMove(m);
+                if (alpha >= beta) {
+                    return alpha;
+                }
+            } 
             return beta;
         }
     }
     
+    Integer bruteForceMax(DraughtsState s, int depth) {
+        if (depth>DEPTH_LIMIT) {
+            return getValue(s);
+        } else {
+            List<Move> moves = s.getMoves();
+            Integer max = Integer.MIN_VALUE;
+            for (Move m : moves) {
+                s.doMove(m);
+                Integer value = bruteForceMin(s, depth + 1);
+                max = (value > max ? value : max);
+                s.undoMove(m);
+            }
+            return max;
+        }
+    }
+    
+    Integer bruteForceMin(DraughtsState s, int depth) {
+        if (depth>DEPTH_LIMIT) {
+            return getValue(s);
+        } else {
+            List<Move> moves = s.getMoves();
+            Integer min = Integer.MAX_VALUE;
+            for (Move m : moves) {
+                s.doMove(m);
+                Integer value = bruteForceMax(s, depth + 1);
+                min = (value > min ? value : min);
+                s.undoMove(m);
+            }
+            return min;
+        }
+    }
 }
